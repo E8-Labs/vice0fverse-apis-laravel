779845c5a006d3b1bd776ee548defcac544c059d
Salman Majid Ali <hello@e8-labs.com>
2023-04-27 14:22:22 +0500

    Is following status added in profile

--- app/Http/Resources/Profile/UserProfileFullResource.php
+++ app/Http/Resources/Profile/UserProfileFullResource.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Models\User;
 use App\Models\User\Follower;
+use Illuminate\Support\Facades\Auth;
 
 class UserProfileFullResource extends JsonResource
 {
